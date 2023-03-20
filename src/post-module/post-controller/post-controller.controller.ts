e016f08fc3e5b577e42dc1d1e6bc5f05deddfcdd
nghiaPTClassFunc <nghiacn1996@gmail.com>
2023-03-20 11:57:06 +0700

    cache with regis

--- src/post-module/post-controller/post-controller.controller.ts
+++ src/post-module/post-controller/post-controller.controller.ts
@@ -1,10 +1,14 @@
+import { CreatePostCommand } from './../commands/createPost.command';
 import { PaginationPostDto } from './../dto/post.dto';
 import { AuthGuard } from '@nestjs/passport';
 import {
   Body,
+  CacheInterceptor,
+  CACHE_MANAGER,
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Post,
   Put,
@@ -12,14 +16,23 @@ import {
   Req,
   UseFilters,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/utils/httpException.filter';
 import { CreatePostDto, UpdatePostDto } from '../dto/post.dto';
 import { PostService } from '../post-service/post-service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { GetPostQuery } from '../queries/getPost.query';
+import { Cache } from 'cache-manager';
 
 @Controller('post')
 export class PostsController {
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @Get()
   getAllPost(@Query() { page, limit, start }: PaginationPostDto) {
     return this.postService.getAllPost(page, limit, start);
@@ -30,10 +43,37 @@ export class PostsController {
   getPostById(@Param('id') id: string) {
     return this.postService.getPostById(id);
   }
+  @Get(':id/get-with-cache')
+  @UseInterceptors(CacheInterceptor)
+  async getPostDetailWithCache(@Param('id') id: string) {
+    console.log('run here');
+    return (await this.postService.getPostById(id)).toJSON();
+  }
+  @Get(':id/get-by-query')
+  async getDetailByQuery(@Param('id') id: string) {
+    return this.queryBus.execute(new GetPostQuery(id));
+  }
+
+  @Get('cache/demo/set-cache')
+  async demoSetCache() {
+    await this.cacheManager.set('newnet', 'helloWord', { ttl: 60 * 10 });
+    return true;
+  }
+
+  @Get('cache/demo/get-cache')
+  async demoGetCache() {
+    return this.cacheManager.get('newnet');
+  }
+
   @Post()
   @UseGuards(AuthGuard('jwt'))
   async createPost(@Req() req: any, @Body() post: CreatePostDto) {
     return this.postService.createPost(req.user, post);
+  }
+  @Post('create-by-command')
+  @UseGuards(AuthGuard('jwt'))
+  async createByCommand(@Req() req: any, @Body() post: CreatePostDto) {
+    return this.commandBus.execute(new CreatePostCommand(req.user, post));
   }
   @Put(':id')
   async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
--- src/post-module/post-module.module.ts
+++ src/post-module/post-module.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { CreatePostHandler } from './handler/createPost.handler';
+import { CacheModule, Module } from '@nestjs/common';
 import { PostService } from './post-service/post-service';
 import { PostsController } from './post-controller/post-controller.controller';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -8,6 +9,11 @@ import { CategorySchema } from './models/category.model';
 import { CategoryRepository } from './repositories/category.repository';
 import { CategoryService } from './post-service/category.service';
 import { CategoryController } from './post-controller/category.controller';
+import { UserModuleModule } from 'src/user-module/user-module.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { GetPostHandler } from './handler/getPost.handler';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -21,8 +27,32 @@ import { CategoryController } from './post-controller/category.controller';
         schema: CategorySchema,
       },
     ]),
+    UserModuleModule,
+    CqrsModule,
+    // CacheModule.register({
+    //   ttl: 10,
+    // }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        // isGlobal: true,
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        username: configService.get<string>('REDIS_USERNAME'),
+        password: configService.get<string>('REDIS_PASSWORD'),
+      }),
+    }),
   ],
-  providers: [PostService, PostRepository, CategoryRepository, CategoryService],
+  providers: [
+    PostService,
+    PostRepository,
+    CategoryRepository,
+    CategoryService,
+    CreatePostHandler,
+    GetPostHandler,
+  ],
   controllers: [PostsController, CategoryController],
 })
 export class PostModule {}
